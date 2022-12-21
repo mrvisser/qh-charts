ca770e285b905f8cc687438af10dece587e11b43
Branden Visser <mrvisser@gmail.com>
2022-12-21 09:52:44 -0500

    reintroduce device adjustment

--- src/services/FreestyleLibreCsvParser.ts
+++ src/services/FreestyleLibreCsvParser.ts
@@ -13,6 +13,37 @@ type CsvRecord = {
   'Scan Glucose mmol/L'?: string;
 };
 
+const transforms: Record<
+  string,
+  { adjust: number; from: string; until?: string }[]
+> = {
+  // Branden test
+  '450E34A9-73C7-43EE-83ED-8D97C25F0B45': [
+    {
+      adjust: 4,
+      from: '2021-03-23T14:01:00Z',
+      until: '2021-03-24T04:00:00Z',
+    },
+  ],
+
+  // Larlkyn
+  '4EF37C42-BC67-4C98-B3AD-A8D69D07AD4F': [
+    {
+      adjust: -1,
+      from: '2022-12-17T05:00:00Z',
+    },
+  ],
+
+  // Larlkyn
+  'B611D004-D7F2-43E0-9D8D-A9C5C0DEBA5A': [
+    {
+      adjust: -1,
+      from: '2022-01-08T05:12:00Z',
+      until: '2022-02-14T05:00:00Z',
+    },
+  ],
+};
+
 export function isFreestyleLibreCsvFile(content: string): boolean {
   return content.startsWith('Glucose Data,');
 }
@@ -24,11 +55,12 @@ export async function parseFreestyleLibreCsvFile(
   const records = await parseCsv(content);
   const locale = resolveLocale(records);
   const data: Record<string, number> = {};
+
   for (const record of records) {
-    const value = parseValue(locale, record);
+    const time = parseTime(locale, record['Device Timestamp'], timezone);
+    const value = parseValue(time, locale, record);
     if (value !== undefined) {
-      const rawTime = record['Device Timestamp'];
-      data[parseTime(locale, rawTime, timezone)] = value;
+      data[time] = value;
     }
   }
 
@@ -51,10 +83,15 @@ function parseTime(locale: CsvLocale, str: string, tz: string): string {
         return 'MM-DD-YYYY hh:mm A';
     }
   })();
-  return moment.tz(str, format, tz).toISOString();
+  const time = moment.tz(str, format, tz).toISOString();
+  return time;
 }
 
-function parseValue(locale: CsvLocale, record: CsvRecord): number | undefined {
+function parseValue(
+  isoTime: string,
+  locale: CsvLocale,
+  record: CsvRecord,
+): number | undefined {
   const value = (() => {
     switch (locale) {
       case 'ca':
@@ -68,7 +105,27 @@ function parseValue(locale: CsvLocale, record: CsvRecord): number | undefined {
     }
   })();
 
-  return !isNaN(value) ? value : undefined;
+  if (isNaN(value)) {
+    return undefined;
+  }
+
+  const serialNumber = record['Serial Number'];
+  if (!(serialNumber in transforms)) {
+    return value;
+  }
+
+  const time = new Date(isoTime).getTime();
+  const adjustment = transforms[serialNumber].find(({ from, until }) => {
+    if (time < new Date(from).getTime()) {
+      return false;
+    } else if (until !== undefined && time > new Date(until).getTime()) {
+      return false;
+    }
+
+    return true;
+  });
+
+  return value + (adjustment?.adjust ?? 0);
 }
 
 function resolveLocale([row]: CsvRecord[]): CsvLocale {
